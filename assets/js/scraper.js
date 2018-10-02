cea559a818312e79caee662a38112232d6fc42de
natebecker11 <natebecker11@gmail.com>
2018-10-02 18:23:21 -0400

    comments

--- assets/js/scraper.js
+++ assets/js/scraper.js
@@ -1,20 +1,22 @@
 
-const fetch = require('node-fetch');
+// // fetch api setup
 
-// import fetch from 'node-fetch';
+// const fetch = require('node-fetch');
+
+// const fetchData = url => {
+//   fetch(url)
+//     .then(resp => resp.json())
+//     .then(resp => console.log(resp['query']['search'][0]['pageid']))
+// }
+// fetchData('https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=1&origin=*&format=json&prop=info&inprop=url&srsearch=Bradley%20Cooper')
+
 
 // let wikiPic = Array.from(document.querySelectorAll('.infobox.biography img'))[0].src
 // let rawZod = Array.from(document.querySelectorAll('a.lienPosition')).filter(elm => elm.href.includes('0'))[0].textContent
 // let rawZod = '25-7 Capricorn';
 // let zod = rawZod.slice(rawZod.indexOf(' ') + 1);
 
-const fetchData = url => {
-  fetch(url)
-    .then(resp => resp.json())
-    // .then(resp => resp['batchcomplete'])
-    // .then(resp => JSON.stringify(resp))
-    .then(resp => console.log(resp['query']['search'][0]['pageid']))
-}
+
 
 // $.ajax({
 //   method: 'GET',
@@ -25,7 +27,6 @@ const fetchData = url => {
 
 
 
-fetchData('https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=1&origin=*&format=json&prop=info&inprop=url&srsearch=Bradley%20Cooper')
 
 // todo:
   // redo ajax in as fetch req
