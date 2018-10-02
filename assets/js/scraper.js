0f1aa399b3c10f1f3c37077f5aba8cb633c788a4
natebecker11 <natebecker11@gmail.com>
2018-10-02 17:57:45 -0400

    scraping wikipedia infos

--- assets/js/scraper.js
+++ assets/js/scraper.js
@@ -1,11 +1,17 @@
+
+// let wikiPic = Array.from(document.querySelectorAll('.infobox.biography img'))[0].src
 // let rawZod = Array.from(document.querySelectorAll('a.lienPosition')).filter(elm => elm.href.includes('0'))[0].textContent
 // let rawZod = '25-7 Capricorn';
 // let zod = rawZod.slice(rawZod.indexOf(' ') + 1);
 
 $.ajax({
   method: 'GET',
-  url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=1&origin=*&format=json&srsearch=Bradley%20Cooper'
-}).then(resp => console.log(resp.query.search[0]))
+  url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=1&origin=*&format=json&prop=info&inprop=url&srsearch=Bradley%20Cooper'
+})
+  // .then(resp => console.log(resp.query.search[0]))
+    .then(resp => console.log(resp))
+
+
 
 
 
