369706ea1f5576505fb70c00ad6ea7bd9d3f5688
natebecker11 <natebecker11@gmail.com>
2018-10-09 21:16:42 -0400

    cheerio setup

---
+++ assets/js/cheeryscrape.js
@@ -0,0 +1,8 @@
+const cheerio = require('cheerio');
+const request = require('request');
+const rp = require('request-promise-native');
+
+
+// const $ = cheerio.load('<h2 class="title">Hello world</h2>')
+
+
